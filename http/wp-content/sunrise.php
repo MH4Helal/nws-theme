ea7c774cf5641ba647cd770fc589a2c79297a277
MH4Helal <52669236+MH4Helal@users.noreply.github.com>
2020-01-17 09:38:13 +0000

    final update

--- http/wp-content/sunrise.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Default mu-plugins directory if you haven't set it
-defined( 'WPMU_PLUGIN_DIR' ) or define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' );
-
-require WPMU_PLUGIN_DIR . '/wp-multisite-same-level-subdomains/wp-multisite-same-level-subdomains.php';
